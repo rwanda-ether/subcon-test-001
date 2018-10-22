ecb84086550e75e789db14f76bbae2c960088b6d
rwanda-ether <rwanda-ether@user.noreply.github.com>
2018-10-22 12:04:13 +0900

    modify plot script by T.H

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
-#Time-stamp: <Mon Oct 22 00:03:58 JST 2018 hamada>
+#Time-stamp: <Mon Oct 22 11:36:51 JST 2018 hamada>
 
 all:
 	git log|grep commit |grep -v initial| tee log.txt
 	./simulator.py |tee Estimations.md
+
+
+install.gnuplot.macOS:
+	brew install gnuplot --with-aquaterm --with-x11
+
--- plot.sh
+++ plot.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env gnuplot
+#Time-stamp: <Mon Oct 22 12:02:38 JST 2018 hamada>
+
+
+set y2tics
+set xlabel "commit"
+
+set log y
+set log y2
+set ylabel "balance [JPY]"
+set y2label "X(n) [JPY]"
+set key left top box lt 3 lw 1 
+set grid
+
+plot "plotdata.log" u 1:4 axis x1y1 w lp lt 1 title "balance [JPY]", \
+     "plotdata.log" u 1:2 axis x1y2 w lp lt 2 title "X(n) [JPY]"
+
+pause -1
 
 set term tgif
 set output "tgif.obj"
+replot
 
-set log y
-plot "plotdata.log" u 1:4 w po
-
-
+set term gif
+set output "img.gif"
+replot
